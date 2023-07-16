1443a9bf5e36311854144412043b17b61be4a65b
Fatih Berksöz <fberksoz>
2023-07-16 22:26:10 +0300

    initial

---
+++ pa-api/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+// User struct represents the user model
+type User struct {
+	ID        int    `json:"id"`
+	Phone     string `json:"phone"`
+	Nickname  string `json:"nickname"`
+	Name      string `json:"name"`
+	Surname   string `json:"surname"`
+	Email     string `json:"email"`
+}
+
+// Geolocation struct represents the geolocation model
+type Geolocation struct {
+	ID        int     `json:"id"`
+	UserID    int     `json:"user_id"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+}
+
+var db *sql.DB
+
+func main() {
+	// Establish database connection
+	connStr := "postgres://park-announce:PosgresDb1591@localhost/PaDB?sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	
+	// Initialize router
+	router := mux.NewRouter()
+	
+	// Define API endpoints
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	
+	// Start the server
+	log.Println("Server started on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+// createUser creates a new user
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	
+	// Insert the user into the database
+	err := db.QueryRow("INSERT INTO users (phone, nickname, name, surname, email) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+	user.Phone, user.Nickname, user.Name, user.Surname, user.Email).Scan(&user.ID)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error creating user", http.StatusInternalServerError)
+		return
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+// getUser retrieves a user by ID
+func getUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+	
+	var user User
+	err := db.QueryRow("SELECT id, phone, nickname, name, surname, email FROM users WHERE id = $1", id).Scan(
+		&user.ID, &user.Phone, &user.Nickname, &user.Name, &user.Surname, &user.Email)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(user)
+	}
+	
+	// updateUser updates a user by ID
+	func updateUser(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		id := params["id"]
+		
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+		
+		// Update the user in the database
+		_, err := db.Exec("UPDATE users SET phone = $1, nickname = $2, name = $3, surname = $4, email = $5 WHERE id = $6",
+		user.Phone, user.Nickname, user.Name, user.Surname, user.Email, id)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error updating user", http.StatusInternalServerError)
+			return
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(user)
+	}
+	
+	// deleteUser deletes a user by ID
+	func deleteUser(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		id := params["id"]
+		
+		// Delete the user from the database
+		_, err := db.Exec("DELETE FROM users WHERE id = $1", id)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error deleting user", http.StatusInternalServerError)
+			return
+		}
+		
+		w.WriteHeader(http.StatusNoContent)
+	}
---
+++ pa-static-site/main.js
@@ -0,0 +1,88 @@
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import { OSM, Vector as sourceVector } from 'ol/source.js';
+import { Vector as layerVector } from 'ol/layer.js';
+import { fromLonLat, toLonLat } from 'ol/proj.js';
+import { Point } from 'ol/geom';;
+import Feature from 'ol/Feature';
+import Icon from 'ol/style/Icon';
+import Style from 'ol/style/Style';
+
+const markerSource = new sourceVector();
+
+
+var markerStyle = new Style({
+    image: new Icon(({
+        scale: 0.3,
+        src: 'https://raw.githubusercontent.com/openlayers/openlayers/v3.20.1/examples/resources/logo-70x70.png',
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 0.75,
+    }))
+});
+
+const map = new Map({
+    layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
+        new layerVector({
+            source: markerSource,
+            style: markerStyle,
+        })
+    ],
+    target: 'map',
+    view: new View({
+        center: fromLonLat([29.0202249, 41.0906537]),
+        zoom: 15,
+    }),
+});
+
+var markerGeometry = new Point(fromLonLat([29.0202249, 41.0906537]));
+var markerFeature = new Feature({
+    geometry: markerGeometry
+});
+
+var markerStyle = new Icon(({
+    src: 'https://github.com/openlayers/openlayers/blob/v3.20.1/examples/resources/logo-70x70.png'
+}));
+
+markerFeature.setStyle(new Style({
+    image: markerStyle,
+}));
+
+var vectorSource = new sourceVector({
+    features: [markerFeature]
+});
+
+var markerLayer = new layerVector({
+    title: "RoutePoint",
+    visible: true,
+    source: vectorSource
+});
+
+//map.layers.addLayer(markerLayer);
+
+
+function addMarker(lon, lat) {
+    console.log('lon:', lon);
+    console.log('lat:', lat);
+
+    var iconFeatures = [];
+
+    var iconFeature = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+        name: 'Null Island',
+        population: 4000,
+        rainfall: 500
+    });
+
+    markerSource.addFeature(iconFeature);
+}
+
+map.on('singleclick', function (event) {
+    var lonLat = toLonLat(event.coordinate);
+    addMarker(lonLat[0], lonLat[1]);
+});
