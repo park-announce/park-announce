33920f012225cebb4d88c24db88f6baa4feab58a
resul guldibi <resulguldibi@192.168.1.65>
2023-07-17 01:10:06 +0300

    000 - resul uptades build.sh file and adds park-announce.drawio file

---
+++ build.sh
@@ -0,0 +1,12 @@
+
+docker build -t park-announce/pa-api pa-api
+docker build -t park-announce/pa-web pa-web
+
+docker-compose up -d
+
+#creare database and table in postgres. insert sample record to postgres table
+docker exec -it db psql -U park-announce -c "create database padb;"
+docker exec -it db psql -U park-announce padb -c "CREATE TABLE users (id SERIAL PRIMARY KEY, phone VARCHAR(20), nickname VARCHAR(50), name VARCHAR(50), surname VARCHAR(50), email VARCHAR(100));"
+docker exec -it db psql -U park-announce padb -c "CREATE TABLE geolocations (id SERIAL PRIMARY KEY, user_id INTEGER, latitude FLOAT, longitude FLOAT, FOREIGN KEY (user_id) REFERENCES users (id));"
+docker exec -it db psql -U park-announce padb -c "INSERT INTO users(phone, nickname, name, surname, email) VALUES ('123456789', 'user1', 'John', 'Doe', 'john.doe@example.com'),('987654321', 'user2', 'Jane', 'Smith', 'jane.smith@example.com');"
+docker exec -it db psql -U park-announce padb -c "INSERT INTO geolocations (user_id, latitude, longitude) VALUES (1, 37.7749, -122.4194),(2, 34.0522, -118.2437);"
