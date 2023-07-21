0cfbd6b446753c178e2ef7a8601f2fe8934207c5
resul guldibi <resulguldibi@192.168.1.69>
2023-07-22 01:11:28 +0300

    000 - resul&fatih adds redis for ditributed lock mechanism

--- pa-api/go.mod
+++ pa-api/go.mod
@@ -1,8 +1,17 @@
 module github.com/park-announce/pa-api
 
-go 1.18
+go 1.20
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/go-redsync/redsync/v4 v4.8.1
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.0.5
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 )
--- pa-api/main.go
+++ pa-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"sort"
@@ -12,6 +13,10 @@ import (
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
+	goredislib "github.com/redis/go-redis/v9"
 )
 
 type Location struct {
@@ -40,6 +45,49 @@ type Geolocation struct {
 var db *sql.DB
 
 func main() {
+
+	// Create a pool with go-redis (or redigo) which is the pool redisync will
+	// use while communicating with Redis. This can also be any pool that
+	// implements the `redis.Pool` interface.
+	client := goredislib.NewClient(&goredislib.Options{
+		Addr: "redis:6379",
+	})
+	pool := goredis.NewPool(client) // or, pool := redigo.NewPool(...)
+
+	// Create an instance of redisync to be used to obtain a mutual exclusion
+	// lock.
+	rs := redsync.New(pool)
+
+	// Obtain a new mutex by using the same name for all instances wanting the
+	// same lock.
+
+	instanceId := 0
+
+	for {
+		mutexname := fmt.Sprintf("global-api-instance-id-%d", instanceId)
+		mutex := rs.NewMutex(mutexname)
+
+		// Obtain a lock for our given mutex. After this is successful, no one else
+		// can obtain the same lock (the same mutex name) until we unlock it.
+		if err := mutex.Lock(); err != nil {
+			instanceId++
+			continue
+		}
+
+		break
+	}
+
+	log.Printf("instanceId : %d", instanceId)
+
+	// Do your work that requires the lock.
+
+	// Release the lock so other processes or threads can obtain a lock.
+	/*
+		if ok, err := mutex.Unlock(); !ok || err != nil {
+			panic("unlock failed")
+		}
+	*/
+
 	// Establish database connection
 	connStr := "postgres://park-announce:PosgresDb1591*@db/padb?sslmode=disable"
 	var err error
