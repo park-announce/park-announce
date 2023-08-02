7c09497d7f76ad349add1f00d22a0a8159bb6926
resul guldibi <resulguldibi@192.168.1.48>
2023-08-03 01:47:36 +0300

    000 - resul updates dump.sql file

--- dump.sql
+++ dump.sql
@@ -73,7 +73,7 @@ CREATE TABLE public.pa_corporation_users (
     id character varying(50) NOT NULL,
     status smallint DEFAULT 1,
     email character varying(200) NOT NULL,
-    password character varying(10) NOT NULL,
+    password character varying(200) NOT NULL,
     corporation_id character varying(50) NOT NULL
 );
 
@@ -124,6 +124,69 @@ CREATE TABLE public.pa_users (
 ALTER TABLE public.pa_users OWNER TO park_announce;
 
 --
+-- Data for Name: pa_corporation_locations; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_corporation_locations (id, corporation_id, geog, status, available_location_count, name) FROM stdin;
+229c5221-0dd6-40a4-b0de-cd63424afeXX	932b7062-6435-4be1-83d1-b37f9d3f0448	0101000020E6100000426D937699043D4094A31074D98C4440	0	12	ispark - uskudar
+\.
+
+
+--
+-- Data for Name: pa_corporation_prices; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_corporation_prices (id, corporation_location_id, price, currency, price_description) FROM stdin;
+229c5221-0dd6-40a4-b0de-cd63424afeYY	229c5221-0dd6-40a4-b0de-cd63424afeXX	20	TL	1 saat
+\.
+
+
+--
+-- Data for Name: pa_corporation_users; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_corporation_users (id, status, email, password, corporation_id) FROM stdin;
+932b7062-6435-4be1-83d1-b37f9d3f0333	1	resulguldibi@gmail.com	$2a$10$E8rQ34gcb/80PT.c1o.WXu22AEcf7BPTqHBsWWfmi1dw9bjNleSAu	932b7062-6435-4be1-83d1-b37f9d3f0448
+5e02217c-b1ae-4da4-8397-f00f5fa9ff72	1	fatihberksoz@gmail.com	$2a$10$CkdA1gV3DhaV38DVDjfmBOnbKAm4bQg4JuqvbWEQFkLrCAxPUi/hm	932b7062-6435-4be1-83d1-b37f9d3f0448
+\.
+
+
+--
+-- Data for Name: pa_corporations; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_corporations (id, name, status) FROM stdin;
+932b7062-6435-4be1-83d1-b37f9d3f0448	ispark	1
+\.
+
+
+--
+-- Data for Name: pa_locations; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_locations (id, geog, status, owner_id, assigned_user_id, scheduled_available_time, location_type) FROM stdin;
+d2c7a5d9-8ae2-4cc7-a14a-4075b308f505	0101000020E61000004C58BBAC7E033D40A06EE384E48C4440	0	932b7062-6435-4be1-83d1-b37f9d3f0448	\N	\N	0
+\.
+
+
+--
+-- Data for Name: pa_users; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.pa_users (id, email, password) FROM stdin;
+932b7062-6435-4be1-83d1-b37f9d3f0448	resulguldibi@gmail.com	\N
+\.
+
+
+--
+-- Data for Name: spatial_ref_sys; Type: TABLE DATA; Schema: public; Owner: park_announce
+--
+
+COPY public.spatial_ref_sys (srid, auth_name, auth_srid, srtext, proj4text) FROM stdin;
+\.
+
+
+--
 -- Name: pa_corporation_locations pa_corporation_locations_pkey; Type: CONSTRAINT; Schema: public; Owner: park_announce
 --
 
@@ -137,6 +200,14 @@ ALTER TABLE ONLY public.pa_corporation_locations
 
 ALTER TABLE ONLY public.pa_corporation_prices
     ADD CONSTRAINT pa_corporation_prices_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: pa_corporation_users pa_corporation_users_email_key; Type: CONSTRAINT; Schema: public; Owner: park_announce
+--
+
+ALTER TABLE ONLY public.pa_corporation_users
+    ADD CONSTRAINT pa_corporation_users_email_key UNIQUE (email);
 
 
 --
